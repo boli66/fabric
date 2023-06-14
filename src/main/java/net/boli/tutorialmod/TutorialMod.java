b95e1cbb2aef0f6263aadc74cff54e47c43ea9f6
boli66 <leo@orr.nu>
2023-06-14 16:15:33 +0200

    registered modded items

--- src/main/java/net/boli/tutorialmod/TutorialMod.java
+++ src/main/java/net/boli/tutorialmod/TutorialMod.java
@@ -1,5 +1,6 @@
 package net.boli.tutorialmod;
 
+import net.boli.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +12,7 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 
 	}
 }
